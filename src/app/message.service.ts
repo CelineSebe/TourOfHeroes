b64d89ff10f8336b0dd38dd6688d241ae374d042
Celine SEBE <celine.sebe@byg4lab.com>
2023-04-02 10:04:28 +0200

    message service

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -8,9 +8,9 @@ export class MessageService {
   messages: string[] = [];
   
   add(message: string){
-    this.messages.push(message)
+    this.messages.push(message);
   }
   clear(){
-    this.messages = []
+    this.messages = [];
   }
 }
